20723694843cf607e018c86cb0b4825e45896f9f
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-05-16 23:25:30 -0500

    fixes

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -38,31 +38,11 @@ export class ProductService {
 
   constructor() {
     this.products = [
-      {
-        id: 1,
-        description: "Product Name 1",
-        price: 111
-      },
-      {
-        id: 2,
-        description: "Product Name 2",
-        price: 222
-      },
-      {
-        id: 3,
-        description: "Product Name 3",
-        price: 333
-      },
-      {
-        id: 4,
-        description: "Product Name 4",
-        price: 444
-      },
-      {
-        id: 5,
-        description: "Product Name 5",
-        price: 555
-      },
+      { id: 1, description: "Smartphone", price: 699.99 },
+      { id: 2, description: "Laptop", price: 1299.99 },
+      { id: 3, description: "Headphones", price: 149.99 },
+      { id: 4, description: "Smartwatch", price: 299.99 },
+      { id: 5, description: "Wireless Speaker", price: 79.99 }
     ]
     this.product = {
       id: -1,
